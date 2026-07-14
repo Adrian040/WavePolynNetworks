f96f7aa85998c8990e3080c5e65c7b7c0874d968
Leonide Lopez Robles <lorl030615@gs.utm.mx>
2026-07-14 12:03:03 -0600

    modificacion en decoder

--- CNN/models/backbones/decoder.py
+++ CNN/models/backbones/decoder.py
@@ -23,11 +23,16 @@ class UpBlock(nn.Module):
     ) -> None:
         super().__init__()
 
-        # Ajusta x al número de canales de las bandas Wavelet.
-        self.reduce_channels = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=1
+        # Ajusta y normaliza la banda LL aprendida.
+        self.reduce_channels = nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=1,
+                bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
         )
 
         # Duplica la resolución usando LL, LH, HL y HH.
